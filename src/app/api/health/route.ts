563899700b7b4a63702ac75ac338653905280558
Coolhgg <129328331+Coolhgg@users.noreply.github.com>
2025-09-29 19:05:52 +0530

    chore: update src/app/api/health/route.ts

--- src/app/api/health/route.ts
+++ src/app/api/health/route.ts
@@ -3,3 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ app: "ok", time: new Date().toISOString() });
 }
+
+export async function HEAD() {
+  return new NextResponse(null, { status: 200 });
+}
